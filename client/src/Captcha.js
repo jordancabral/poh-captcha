184c8f1ea7fe50fd885c65f95fd66d27d775e20b
jordancabral <jorcabral@falabella.com.ar>
2022-02-08 16:30:21 -0300

    added ethers to FE

--- client/src/Captcha.js
+++ client/src/Captcha.js
@@ -1,90 +1,74 @@
 import React from "react";
-import getWeb3 from "./getWeb3";
 import POH from "./contracts/ProofOfHumanity.json";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Loader from "react-loader-spinner";
+import { ethers } from "ethers";
 import "./Captcha.css"
 
 const Captcha = props => {
 
-    const [web3, setWeb3] = useState(null);
-    const [accounts, setAccounts] = useState(null);
-    const [contract, setContract] = useState(null);
     const [photo, setPhoto] = useState("https://app.proofofhumanity.id/images/governance.png");
     const [checked, setChecked] = useState(null);
     const [loading, setLoading] = useState(false);
-  
-    const loadContract = async () => {
-      try {
 
-        // Get network provider and web3 instance.
-        const web3 = await getWeb3();
-        setWeb3(web3);
-  
-        // Use web3 to get the user's accounts.
-        const accounts = await web3.eth.getAccounts();
-        setAccounts(accounts);
-  
-        // Get the contract instance.
-        const networkId = await web3.eth.net.getId();
-        const deployedNetwork = POH.networks[networkId];
-        const contract = new web3.eth.Contract(
-          POH.abi,
-          deployedNetwork && deployedNetwork.address
-        );
-        setContract(contract);
-  
-      } catch (error) {
-        // Catch any errors for any of the above operations.
-        alert(
-          `Failed to load web3, accounts, or contract. Check console for details.`
-        );
-        console.error(error);
-      }
-    };
 
     const validateAdress = async () => {
 
-        console.log('checked')
-        setLoading(true)
+      setLoading(true)
+
+      try {
+        // Check if wallet is installed
+        if (!window.ethereum)
+          throw new Error("No crypto wallet found. Please install it.");
+    
+        // A Web3Provider wraps a standard Web3 provider, which is
+        // what MetaMask injects as window.ethereum into each page
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+        // MetaMask requires requesting permission to connect users accounts
+        await provider.send("eth_requestAccounts", []);
+
+        const signer = await provider.getSigner();
+        const address = await signer.getAddress();
+
+        // Load contract
+        const network = await provider.getNetwork()
+        const networkId = network.chainId
+        
+        const pohNetwork = POH.networks[networkId];
+        const pohContract = new ethers.Contract(pohNetwork.address, POH.abi, provider);
 
         // Get info of user
-        const response = await contract.methods.isRegistered(accounts[0]).call();
+        const response = await pohContract.isRegistered(address);
         if (!response){
-            props.handleWrong();
+          props.handleWrong();
         } else {
-    
-            const message = web3.utils.randomHex(32)
-            // Signs the messageHash with a given account
-            const signature = await web3.eth.personal.sign(message, accounts[0]);
-            const sig = {
-                address: accounts[0],
-                message,
-                signature 
-            }
-            console.log(sig)
-        
-            // Get Info from API
-            getProfileInfo(accounts[0])
+          //const message = web3.utils.randomHex(32)
+          const message = "test" // Todo random msg
 
-            setLoading(false)
-            setChecked(true);
+          // Signs the message
+          const signature = await signer.signMessage(message);
+          const sig = {
+              address,
+              message,
+              signature 
+          }
+          console.log(sig)
+      
+          // Get Info from API
+          getProfileInfo(address)
 
-            props.handleRight(Buffer.from(JSON.stringify(sig)).toString('base64'));
-    
-            // // Backend validation test (this goes in backend)
-            // const signer = await web3.eth.personal.ecRecover(message, signature);
-            // console.log('signer ' + signer)
-            // if (web3.utils.toChecksumAddress(signer) === web3.utils.toChecksumAddress(sig.address)) {
-            //     console.log('Signature OK')  
-            // } else {
-            //     console.log('Signature WRONG')  
-            // }
-    
+          setLoading(false)
+          setChecked(true);
+
+          props.handleRight(Buffer.from(JSON.stringify(sig)).toString('base64'));
         }
-    
-    
+
+      } catch (err) {
+          // TODO: props handle error 
+          console.error(err.message);
       }
+    };
 
     const getProfileInfo = (address) => {
         fetch(`https://api.poh.dev/profiles/${address}`)
@@ -97,14 +81,6 @@ const Captcha = props => {
               console.error(error)
           }
         )
-    }
-
-    useEffect(() => {
-        loadContract();
-    }, []);
-
-    if (!web3) {
-        return <div>Loading Web3, accounts, and contract...</div>;
     }
 
     return (
