9182ee3382856772540dd7fdd52b99e1bdd8ad2e
jordancabral <jorcabral@falabella.com.ar>
2022-02-08 16:48:09 -0300

    added ethers to BE

--- service/src/app.controller.ts
+++ service/src/app.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, Post, Headers, BadRequestException } from '@nestjs/common';
 import { AppService } from './app.service';
-import { recoverPersonalSignature } from 'eth-sig-util';
+import { ethers } from 'ethers';
 
 @Controller()
 export class AppController {
@@ -29,34 +29,26 @@ export class AppController {
     const decodedCaptcha = Buffer.from(captcha, 'base64').toString('utf8');
     const sig = JSON.parse(decodedCaptcha);
 
-
-
-    const msgParams = {
-      data: sig.message,
-      sig: sig.signature,
-    };
-    const signer = recoverPersonalSignature(msgParams);
-    // console.log(sig.address);
-    // console.log(signer);
-
-    if (signer.toLowerCase() == sig.address.toLowerCase()) {
-      console.log('Signature OK');
-    } else {
-      console.log('Signature WRONG');
-    }
-
     try {
-      const response = this.appService.findAddress(signer);
+      const signerAddr = await ethers.utils.verifyMessage(sig.message, sig.signature);
+      if (signerAddr.toLowerCase() == sig.address.toLowerCase()) {
+        console.log('Signature OK');
+      } else {
+        console.log('Signature WRONG');
+        throw new BadRequestException('Wrong Signature');
+      }
+
+      const response = this.appService.findAddress(signerAddr);
       await response.forEach((value) => {
         if (value.status == 200) {
           console.log('Human registered in POH');
         } else {
           console.log('Not an Human registered in POH');
+          throw new BadRequestException('Not an Human registered in POH');
         }
       });
     } catch (e) {
-      //console.log(e);
-      throw new BadRequestException('Wrong POH captcha');
+      throw new BadRequestException('Error trying to check signature');
     }
     return sig;
   }
